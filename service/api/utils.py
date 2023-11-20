547998a646d2aa9fe8a2dabe62402e7afafc955f
alpotekhin <al_potechin@mail.ru>
2023-11-21 01:05:03 +0300

    authentication added

---
+++ service/api/utils.py
@@ -0,0 +1,5 @@
+def get_model_names():
+    existing_models = [
+        'dummy'
+    ]
+    return  existing_models
--- service/api/views.py
+++ service/api/views.py
@@ -1,12 +1,14 @@
 import random
 from typing import List
 
-from fastapi import APIRouter, FastAPI, Request, status
+from fastapi import APIRouter, FastAPI, Request, status, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel
 
 from service.api.exceptions import ModelNotFoundError, UserNotFoundError
 from service.log import app_logger
-
+from service.api.utils import get_model_names
+from service.api.credentials import get_token
 
 class RecoResponse(BaseModel):
     user_id: int
@@ -14,13 +16,23 @@ class RecoResponse(BaseModel):
 
 
 router = APIRouter()
+bearer_scheme = HTTPBearer()
+secret_token = get_token()
 
+
+async def read_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+) -> str:
+    if credentials.credentials == secret_token:
+        return credentials.credentials
+    raise ValueError()
 
 @router.get(
     path="/health",
     tags=["Health"],
+
 )
-async def health() -> str:
+async def health(token: str = Depends(read_current_user)) -> str:
     return "I am alive"
 
 
@@ -34,20 +46,23 @@ async def get_reco(
     request: Request,
     model_name: str,
     user_id: int,
+    token: str = Depends(read_current_user)
 ) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
     # Write your code here
-    if model_name == "dummy":
-        reco = random.sample(range(100), 10)
-    else:
+    existing_models = get_model_names()
+    
+    if model_name not in existing_models:
         raise ModelNotFoundError(error_message=f"Model {model_name} not found")
         
-
     if user_id > 10**9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
-
-    k_recs = request.app.state.k_recs
+    
+    if model_name == "dummy":
+        reco = random.sample(range(100), 10)
+    
+    # k_recs = request.app.state.k_recs
     # reco = list(range(k_recs))
     return RecoResponse(user_id=user_id, items=reco)
 
